350a17a1ec1217e9e0c3855c38b33adacf954ad4
Yurika Mac <macusaya.a.yurika@gmail.com>
2022-08-11 20:36:02 -0400

    my first commit

---
+++ firstpyapp.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from PIL import image
+
+print('1st web app')
+
+
+txt = st.text_area('Text to analyze', '''
+     The Walrus or := operator is one of the latest additions 
+     to python 3.8. It is an assignment operator that lets you 
+     assign value to a variable within an expression like 
+     conditional statements, loops, etc
+     ''')
+st.write('1. Walrus operator', run_sentiment_analysis(txt))
+
+
+txt = st.text_area('another text to test', '''
+     hey there, Am i below?
+     ''')
+st.write('2. not sure', run_sentiment_analysis(txt))
