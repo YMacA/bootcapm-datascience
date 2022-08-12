ca01dff392aab35ef3a01b29b2a17c1cf4bd20bc
Yurika Mac <macusaya.a.yurika@gmail.com>
2022-08-11 20:55:19 -0400

    my 3rd commit

--- firstpyapp.py
+++ firstpyapp.py
@@ -1,19 +1,27 @@
 import streamlit as st
 from PIL import Image
+import pandas as pd
 
-st.write('1st web app')
-
-
-txt = st.text_area('Text to analyze', '''
-     The Walrus or := operator is one of the latest additions 
-     to python 3.8. It is an assignment operator that lets you 
-     assign value to a variable within an expression like 
-     conditional statements, loops, etc
-     ''')
-st.write('1. Walrus operator', run_sentiment_analysis(txt))
+st.write('10 Cool Beginner Python Tricks That Will Make Your Life Easier')
 
 
-txt = st.text_area('another text to test', '''
-     hey there, Am i below?
-     ''')
-st.write('2. not sure', run_sentiment_analysis(txt))
+image = Image.open('minion.jpg')
+
+st.image(image, caption='Sunrise by the mountains')
+
+st.write('st.dataframe Display a dataframe as an interactive table.')
+
+df = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)  # Same as st.write(df)
+
+st.dataframe(df, 200, 100)
+st.write('You can also pass a Pandas Styler object to change the style of the rendered DataFrame:')
+df = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df.style.highlight_max(axis=0))
+
